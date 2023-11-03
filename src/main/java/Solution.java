b23951edc746705ce495e6cb79b92d50eab35a06
alek <wait82985@gmail.com>
2023-11-03 13:10:32 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,7 +1,25 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
     public List<String> buildArray(int[] target, int n) {
-        return null;
+        List<String> result = new ArrayList<>();
+        int j = 1;
+        for (int i = 0; i < target.length; i++) {
+            int popCount = 0;
+            while (target[i] != j) {
+                j++;
+                popCount++;
+                result.add("Push");
+            }
+            while (popCount != 0) {
+                result.add("Pop");
+                popCount--;
+            }
+            result.add("Push");
+            j++;
+
+        }
+        return result;
     }
 }
